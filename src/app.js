b618279d829c2f7fe76aaec370a51e594b5b6a96
ItsEugenio <223730@ids.upchiapas.edu.mx>
2024-03-07 23:11:38 -0600

    add espress rate limit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import express from "express";
 import morgan from "morgan"; //middleware
 import cors from "cors"
 import TasksRoutes from "./routes/tasks.routes";
+
 const app = express();
+
+
+
 
 app.set("port", process.env.PORT || 3000);
 
@@ -12,10 +16,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); //permite peticiones desde HTML
 
-app.get("/", (req, res) => {
+
+
+app.get("/",(req, res) => {
   res.json({ message: "esta es mi api" });
 });
 
-app.use("/api/tasks", TasksRoutes);
+app.use("/api/tasks",TasksRoutes);
+
 
 export default app;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,15 +2,25 @@ import { Router } from "express";
 
 import * as taskController from "../controllers/task.controller";
 
+const rateLimit = require("express-rate-limit");
+
+const accountLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 6, 
+    message: "Demasiadas peticiones realizadas, intenta despues de 1 hora"
+  });
+
 const router = Router();
 
-router.get("/", taskController.getAllTasks);
 
-router.post("/", taskController.createTask);
+
+router.get("/", accountLimiter,taskController.getAllTasks);
+
+router.post("/", accountLimiter,taskController.createTask);
 
 router.get("/done", taskController.findAllDoneTask);
 
-router.get("/:id", taskController.findOneTask);
+router.get("/:id", accountLimiter,taskController.findOneTask);
 
 router.delete("/:id", taskController.deleteTask);
 
